7c860f0d906964c6acbb5e8c727bf11724c236a7
Kim Valkama <hello@valkama.kim>
2026-01-17 21:15:00 +0200

    Added gpu tmp check

--- gpu.c
+++ gpu.c
@@ -14,6 +14,17 @@ int main() {
     } else {
       fprintf(stderr, "Failed to retrieve GPU usage for device %u\n", gpu_id);
     }
+
+    uint64_t temp;
+    if ((status = rsmi_dev_temp_metric_get(gpu_id, 0, RSMI_TEMP_CURRENT,
+                                           (int64_t *)&temp)) ==
+        RSMI_STATUS_SUCCESS) {
+      printf("GPU %u Temperature: %lu°C\n", gpu_id,
+             temp / 1000); // Temperature is usually reported in millidegrees C
+    } else {
+      fprintf(stderr, "Failed to retrieve GPU temperature for device %u\n",
+              gpu_id);
+    }
     rsmi_shut_down();
   } else {
     fprintf(stderr, "Failed to initialize ROCm SMI (Error Code: %d)\n", status);
