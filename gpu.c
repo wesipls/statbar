6ce218baedc813b8c1ef48b2470247afc6dc5df9
Kim Valkama <hello@valkama.kim>
2026-01-17 21:35:51 +0200

    Some refactoring

--- gpu.c
+++ gpu.c
@@ -8,26 +8,25 @@ int main() {
   uint32_t gpu_busy_percent;
   uint32_t gpu_id = 0;
 
-  if ((status = rsmi_init(RSMI_INIT_FLAG_ALL_GPUS)) == RSMI_STATUS_SUCCESS) {
-    if ((status = rsmi_dev_busy_percent_get(gpu_id, &gpu_busy_percent)) ==
-        RSMI_STATUS_SUCCESS) {
-      uint64_t temp;
-      if ((status = rsmi_dev_temp_metric_get(gpu_id, 0, RSMI_TEMP_CURRENT,
-                                             (int64_t *)&temp)) ==
-          RSMI_STATUS_SUCCESS) {
-        printf("GPU: %u%% +%lu°C\n", gpu_busy_percent, temp / 1000);
-      } else {
-        fprintf(stderr, "GPU %u: Usage %u%%, Failed to retrieve temperature\n",
-                gpu_id, gpu_busy_percent);
-      }
-    } else {
-      fprintf(stderr, "Failed to retrieve GPU usage for device %u\n", gpu_id);
+  if ((status = rsmi_init(RSMI_INIT_FLAG_ALL_GPUS)) != RSMI_STATUS_SUCCESS) {
+      fprintf(stderr, "Failed to initialize ROCm SMI (Error Code: %d)\n", status);
+      return -1;
     }
+  
+    if ((status = rsmi_dev_busy_percent_get(gpu_id, &gpu_busy_percent)) != RSMI_STATUS_SUCCESS) {
+      fprintf(stderr, "Failed to retrieve GPU usage for device %u\n", gpu_id);
+      rsmi_shut_down();
+      return -1;
+    }
+  
+    uint64_t temp;
+    if ((status = rsmi_dev_temp_metric_get(gpu_id, 0, RSMI_TEMP_CURRENT, (int64_t *)&temp)) != RSMI_STATUS_SUCCESS) {
+      fprintf(stderr, "GPU %u: Usage %u%%, Failed to retrieve temperature\n", gpu_id, gpu_busy_percent);
+      rsmi_shut_down();
+      return -1;
+    }
+  
+    printf("GPU: %u%% +%lu°C\n", gpu_busy_percent, temp / 1000);
     rsmi_shut_down();
-  } else {
-    fprintf(stderr, "Failed to initialize ROCm SMI (Error Code: %d)\n", status);
-    return -1;
-  }
-
   return 0;
 }
