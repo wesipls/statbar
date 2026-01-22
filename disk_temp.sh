0989f8c92b7c39cd4a57cc2043e7083af268fd96
Kim Valkama <hello@valkama.kim>
2026-01-22 19:43:57 +0200

    Fixed disk_temp.sh

--- disk_temp.sh
+++ disk_temp.sh
@@ -5,9 +5,10 @@
 #
 # Fetch and display the temperature of $disk
 #
-# Assumes disk is /dev/sdb, modify as needed.
+# Change $disk to the uuid of the disk you want to get temperature for.
 #
 # Should work on most Linux systems with smartmontools installed.
+# Needs sudo permissions for smartmontools to access disk information, to add it for just smartmontools add this to the /etc/sudoers file: ALL=(ALL) NOPASSWD: /usr/sbin/smartctl
 
-disk=sdb
-sudo smartctl -a /dev/$disk | grep Temperature | awk -F " " '{print " +"$10 "°C"}'
+disk=8b33bb8a-2621-4d06-9645-9809f09e660b
+sudo smartctl -a /dev/disk/by-uuid/$disk | grep Temperature | awk -F " " '{print " +"$10 "°C"}'
