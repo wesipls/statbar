256d10a7d13772b726fe9c9b29ab0491689ab3d8
Kim Valkama <hello@valkama.kim>
2026-01-22 19:48:37 +0200

    Fixed comments

--- disk.c
+++ disk.c
@@ -1,21 +1,27 @@
+/*
+  Usage: ./disk
+  Example Output: DISK: 45%
+
+  Not much to say here, statvfs is included in most distros by default.
+  If you want to measure a different mount point, change the "/" in statvfs on
+  line 15.
+*/
+
 #include <stdio.h>
 #include <sys/statvfs.h>
 
 int main() {
   struct statvfs stat;
 
-  // Point to the root directory or other mount point
   if (statvfs("/", &stat) != 0) {
     perror("statvfs failed");
     return 1;
   }
 
-  // Calculate disk usage in percentage
   unsigned long total_blocks = stat.f_blocks;
   unsigned long free_blocks = stat.f_bfree;
   double used_percentage = 100.0 * (1.0 - ((double)free_blocks / total_blocks));
-  int rounded_used_percentage =
-      (int)(used_percentage + 0.5); // Round to nearest integer
+  int rounded_used_percentage = (int)(used_percentage + 0.5);
 
   printf("DISK: %d%%\n", rounded_used_percentage);
 
